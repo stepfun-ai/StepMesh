78b7fe49db47ee293a4fd3cd867635e07c584627
Yulu Jia <62443743+pleasantrabbit@users.noreply.github.com>
2021-11-26 08:44:43 -0800

    rdma van: mr leak in connection retry (#108)
    * rdma van: mr leak in connection retry
    
    do not allocate new MRs during connection retry.
    
    Signed-off-by: Yulu Jia <yulu.jia@bytedance.com>

--- tools/check_diff.sh
+++ tools/check_diff.sh
@@ -3,7 +3,7 @@ set -e
 
 bash tools/format_code.sh
 
-git diff --stat
+git diff
 diff_lines=$(git diff --stat|wc -l)
 if [[ $diff_lines -gt 0 ]]; then
   echo "Found diff: $diff_lines. run tools/format_code.sh to format your code first."
